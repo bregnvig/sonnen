b47fec2ec646f2ef30fcb2d8fe2d01059f58186a
Flemming Bregnvig <flemming@bregnvig.dk>
2025-04-07 15:08:48 +0200

    Add boot and destroy message

--- apps/pi/src/app/app.module.ts
+++ apps/pi/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { Logger, Module } from '@nestjs/common';
+import { Logger, Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
+import { collectionPath, SonnenEvent } from '@sonnen/data';
 import { isNullish } from '@sonnen/utils';
+import { firestore } from 'firebase-admin';
 import { of, switchMap } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule, SonnenService } from './common';
-import { FirestoreModule } from './firestore';
+import { FirestoreModule, FirestoreService } from './firestore';
 import { SimpleBatteryCheckModule } from './simple-battery-check';
 import { StatusModule } from './status';
 
@@ -24,11 +26,31 @@ import { StatusModule } from './status';
     AppService,
   ],
 })
-export class AppModule {
-  constructor(service: SonnenService) {
+export class AppModule implements OnModuleInit, OnModuleDestroy {
+  constructor(service: SonnenService, private firestore: FirestoreService) {
     const logger = new Logger(AppModule.name);
     service.isManual().pipe(
       switchMap(isManual => isManual ? service.automaticMode() : of(undefined)),
     ).subscribe(value => logger.log(isNullish(value) ? 'Already in automatic mode' : 'Changed to automatic mode'));
+  }
+
+  async onModuleInit() {
+    const db = this.firestore.db;
+    await db.collection(collectionPath.events).add({
+      message: `Sonnen PI has started successfully`,
+      timestamp: firestore.Timestamp.now(),
+      source: `AppModule:Ready`,
+      type: 'info',
+    } as SonnenEvent);
+  }
+
+  async onModuleDestroy() {
+    const db = this.firestore.db;
+    await db.collection(collectionPath.events).add({
+      message: `Sonnen PI is shutting down`,
+      timestamp: firestore.Timestamp.now(),
+      source: `AppModule:Destroy`,
+      type: 'info',
+    } as SonnenEvent);
   }
 }
