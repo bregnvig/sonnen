1c0148f45d56c4cba00d21071dde5b0e5afbc897
Flemming Bregnvig <flemming@bregnvig.dk>
2025-04-14 17:00:43 +0200

    Add timestamp

--- libs/ui/events/src/lib/events/events.component.ts
+++ libs/ui/events/src/lib/events/events.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule, TitleCasePipe } from '@angular/common';
+import { CommonModule, DatePipe, TitleCasePipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
@@ -7,7 +7,7 @@ import { CardPageComponent } from '@sonnen/common';
 
 @Component({
   selector: 'events-page',
-  imports: [CommonModule, CardPageComponent, MatCard, MatCardHeader, MatCardContent, MatCardTitle, TitleCasePipe],
+  imports: [CommonModule, CardPageComponent, MatCard, MatCardHeader, MatCardContent, MatCardTitle, TitleCasePipe, DatePipe],
   template: `
     <common-card-page>
       @for (event of events(); track $index) {
@@ -15,11 +15,13 @@ import { CardPageComponent } from '@sonnen/common';
           <mat-card-header>
             <mat-card-title>
               {{ event.type | titlecase }}
-
             </mat-card-title>
           </mat-card-header>
           <mat-card-content>
-            {{ event.message }}
+            <div class="flex flex-col gap-2">
+              <p>{{ event.message }}</p>
+              <small class="text-tiny">{{ +event.timestamp | date: 'short' }}</small>
+            </div>
           </mat-card-content>
         </mat-card>
       }
