cd7f33df74132aaefb6a7da9b867c719696b546f
Flemming Bregnvig <flemming@nestech.dk>
2025-04-15 21:00:27 +0200

    Add production service

--- apps/pi/src/app/status/status.module.ts
+++ apps/pi/src/app/status/status.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from '../common';
 import { FirebaseModule } from '../firebase';
 import { ConsumptionCronService } from './consumption.cron';
+import { ProductionCronService } from './production.cron';
 
 @Module({
   imports: [CommonModule, FirebaseModule],
-  providers: [ConsumptionCronService],
+  providers: [ConsumptionCronService, ProductionCronService],
 })
-export class StatusModule {}
+export class StatusModule { }
