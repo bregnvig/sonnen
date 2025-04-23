27268fb7d659dec2f7712732e81a3e1b6a19dd6a
Flemming Bregnvig <flemming@bregnvig.dk>
2025-04-23 11:05:14 +0200

    Add daily cost info

---
+++ apps/pi/src/app/status/cost.cron.ts
@@ -0,0 +1,35 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { SonnenEvent } from '@sonnen/data';
+import { firstValueFrom } from 'rxjs';
+import { CostService, EventService } from '../common';
+
+@Injectable()
+export class CostCronService {
+  #logger = new Logger(CostCronService.name);
+
+  constructor(private service: CostService, private eventService: EventService) {
+    this.#logger.debug('CostCronService started');
+  }
+
+  @Cron(CronExpression.EVERY_DAY_AT_1AM)
+  async reportCost() {
+    const cost = await firstValueFrom(this.service.getPrices());
+
+    const minPrice = cost.reduce((min, item) => !min || item.kWh <= min.kWh ? item : min, undefined);
+    const maxPrice = cost.reduce((max, item) => !max || item.kWh >= max.kWh ? item : max, undefined);
+
+    const message: SonnenEvent = {
+      type: 'info',
+      source: `${CostCronService.name}:CostInfo`,
+      message: `Minimums pris ${minPrice.kWh} kr/kWh kl. ${minPrice.from.toFormat('HH:mm')} - Maximums pris ${maxPrice.kWh} kr/kWh kl. ${maxPrice.from.toFormat('HH:mm')}`,
+      data: [
+        minPrice,
+        maxPrice,
+      ],
+    };
+    await this.eventService.add(message);
+    await this.eventService.sendToUsers('Dagens priser', message.message);
+
+  }
+}
--- apps/pi/src/app/status/status.module.ts
+++ apps/pi/src/app/status/status.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '../common';
 import { FirebaseModule } from '../firebase';
 import { BatteryCronService } from './battery.cron';
 import { ConsumptionCronService } from './consumption.cron';
+import { CostCronService } from './cost.cron';
 import { ProductionCronService } from './production.cron';
 
 @Module({
   imports: [CommonModule, FirebaseModule],
-  providers: [ConsumptionCronService, ProductionCronService, BatteryCronService],
+  providers: [ConsumptionCronService, ProductionCronService, BatteryCronService, CostCronService],
 })
 export class StatusModule {}
