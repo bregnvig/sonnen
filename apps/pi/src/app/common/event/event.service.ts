fb7f2c68ecd6ce2af12b2f7997469ddfbbd7673e
Flemming Bregnvig <flemming@bregnvig.dk>
2025-11-02 14:25:19 +0100

    Handle messaging errors

--- apps/pi/src/app/common/event/event.service.ts
+++ apps/pi/src/app/common/event/event.service.ts
@@ -10,7 +10,8 @@ export class EventService {
 
   #logger = new Logger(EventService.name);
 
-  constructor(private firebase: FirebaseService) {}
+  constructor(private firebase: FirebaseService) {
+  }
 
   async add(event: SonnenEvent) {
     return this.firebase.db.collection('events').withConverter(converter).add(({
@@ -24,10 +25,16 @@ export class EventService {
       value => value.docs.map(d => d.data() as User),
     );
     const tokes = users.flatMap(u => u.tokens ?? []);
-    this.#logger.debug(`Sending notification to ${tokes.length} users`);
+    this.#logger.debug(`Sending notification to ${ tokes.length } users`);
     while (tokes.length > 0) {
       const token = tokes.pop();
-      await this.firebase.sendToToken(token, title, message);
+      try {
+        await this.firebase.sendToToken(token, title, message);
+      } catch (error) {
+        const user = users.find(u => u.tokens.includes(token));
+        this.#logger.warn(`Unable to send to ${ token }. User ${ user?.displayName ?? 'Unknown' }`);
+        this.#logger.error(error);
+      }
     }
 
   }
